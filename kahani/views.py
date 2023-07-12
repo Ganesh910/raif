84662995a9635f1e495a3e283625b680a1e007d3
pandeyganesh <pandeyganesha@gmail.com>
2023-07-13 01:04:41 +0530

    Some minor updates

--- kahani/views.py
+++ kahani/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Kahani
 
 # Create your views here.
 
 def index(request):
-    return render(request, "kahani/index.html")
+    kahani = Kahani.objects.all()
+    return render(request, "kahanis/index.html", {
+        "kahanis":kahani
+    })
+
+# It will render the page for a story. Take id for the kahani as input
+def view_kahani(request, kahani_id):
+
+    # get me the story from the models with this id
+    kahani = Kahani.objects.get(id=kahani_id)
+
+    # render the kahani.html page and pass the values of kahani
+    return render(request, "kahanis/kahani.html", {
+        "kahani":kahani
+    })
