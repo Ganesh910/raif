6e9e66c3b5a2a7c57f8161cd8bdcd5eb98767540
pandeyganesh <pandeyganesha@gmail.com>
2023-07-13 01:51:57 +0530

    added app_name to avoid namespace collisions

--- kahani/urls.py
+++ kahani/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+# To avoid namespace collisions
+app_name  = "kahani"
 urlpatterns = [
     path("", views.index, name='index'),
-    path("<int:kahani_id>", views.view_kahani, name="kahani")
+    path("<int:kahani_id>", views.view_kahani, name="post")
 ]
