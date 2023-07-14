1e275aac9d3464ba74d15bd188b51e8c94a15248
pandeyganesh <pandeyganesha@gmail.com>
2023-07-14 16:33:29 +0530

    created index and post views for nazm and added to the layout.html

--- nazm/urls.py
+++ nazm/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name="nazm"
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("<int:post_id>", views.view_nazm, name='nazm')
 ]
--- nazm/views.py
+++ nazm/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Nazm
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Online!!")
-    # return render(request, 'index.html')
+    nazms = Nazm.objects.all()
+    context = {
+        'posts':nazms,
+        'app_name':'nazm',
+        'path_name':'nazm'
+    }
+    return render(request, 'index.html', context)
+
+def view_nazm(request, post_id):
+    nazm = Nazm.objects.get(id=post_id)
+    context = {
+        'post':nazm
+    }
+    return render(request, 'post.html', context)
