54c89d74837072545ac96253beab778736f099a4
pandeyganesh <pandeyganesha@gmail.com>
2023-07-14 16:23:22 +0530

    Created nazm model

--- nazm/models.py
+++ nazm/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
 # Create your models here.
+
+class Nazm(models.Model):
+    date_written = models.DateTimeField(auto_now_add=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
