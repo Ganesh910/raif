c50863957cea92b4c619d3af5860fae70e835786
pandeyganesh <pandeyganesha@gmail.com>
2023-07-15 18:41:23 +0530

    added staticfiles and added path in settings.py

--- raif/settings.py
+++ raif/settings.py
@@ -122,10 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #Add this in your settings.py file:
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
