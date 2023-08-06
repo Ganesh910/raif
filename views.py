7e5064b070c0e36df1c3066a4fb3107dcf56d7a3
pandeyganesh <pandeyganesha@gmail.com>
2023-08-06 14:16:34 +0530

    Updated text for homepage and about

--- views.py
+++ views.py
@@ -6,7 +6,7 @@ def about(request):
     context = {
         'post':{'title':'Raif',
                 'date_written':"Urf Ganesh Pandey",
-                 'content':"Kya janna chahte ho mere baare mein? \n You are here, it means you already know me. So go and just read my poems. \n \n And yes, don't forget to DM me to say ki wah Ganesh kya likhta hai tu. Bs pr dil se khna hai tumhe, let's assume I haven't asked you to say this to me :) " }
+                 'content':"Step into my poetic realm! I'm Ganesh Pandey, a Computer Engineering student who loves to express through heartfelt verses. \n\nIn the world of codes and tech, I found a beautiful escape in poetry. Words became my canvas to paint emotions and dreams.\n\nHere, you'll find my poems, each a glimpse into my heart. From nature's wonders to human connections, I explore it all.\n\nLet's embark on this journey together, where technology and poetry unite. I hope my words touch your soul.\n\nWelcome and enjoy!" }
     }
     return render(request, 'post.html', context)
 
@@ -15,7 +15,7 @@ def home(request):
         'post':{
         'title':"Raif Writes",
         'date_written':"This Is Home (page)",
-        'content':"Mitr! I am too busy right now! I'll make a different Home page later. Tb tk tum Navigation Bar se Kaam chala lo please. \n Don't forget to check out About page, I've left a special message for you there ;)"
+        'content':"While my homepage is still taking shape, I invite you to join me on this voyage of words and emotions. Soon, this space will be filled with the magic of poetry, where nature's beauty and human experiences intertwine. \n\nIn the meantime, stay tuned, for I am crafting each verse with care, just for you. Let's embark on this poetic adventure together, celebrating the harmony of technology and the art of expression. \n\nThank you for your patience, and I can't wait to share my poems with you soon!"
         }
     }
     return render(request, 'post.html', context)
